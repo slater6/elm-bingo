d1615fa8adf2471c1102a4c0f87e2aa700059348
Bryan Rodrigues <slaters@gmail.com>
2017-01-17 14:06:03 -0500

    Setup Model and Aliases

--- starter-project/bingo/Bingo.elm
+++ starter-project/bingo/Bingo.elm
@@ -4,6 +4,38 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+-- Model
+
+
+type alias Model =
+    { name : String, gameNumber : Int, entries : List Entry }
+
+
+type alias Entry =
+    { id : Int, phrase : String, points : Int, marked : Bool }
+
+
+initialModel : Model
+initialModel =
+    { name = "Mike"
+    , gameNumber = 1
+    , entries = initialEntries
+    }
+
+
+initialEntries : List Entry
+initialEntries =
+    [ Entry 1 "Test" 100 False
+    , Entry 2 "Doing Agile" 200 False
+    , Entry 3 "In the cloud" 300 False
+    , Entry 4 "Rock-Star Ninja" 400 False
+    ]
+
+
+
+-- View
+
+
 playerInfo : String -> Int -> String
 playerInfo name gameNumber =
     name ++ " - Game # " ++ (toString gameNumber)
@@ -32,11 +64,20 @@ viewFooter =
         [ a [ href "http://elm-lang.org" ] [ text "Powered By Elm" ] ]
 
 
-view : Html msg
-view =
-    div [ class "content" ] [ viewHeader "BUZZWORD BINGO", viewPlayer "Nicole" 4, viewFooter ]
+
+-- view : Html msg
+
+
+view : Model -> Html msg
+view model =
+    div [ class "content" ]
+        [ viewHeader "BUZZWORD BINGO"
+        , viewPlayer model.name model.gameNumber
+        , div [ class "debug" ] [ text (toString model) ]
+        , viewFooter
+        ]
 
 
 main : Html msg
 main =
-    view
+    view initialModel
